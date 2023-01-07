1d82511b50bca65e68271b91255eae622f8053c7
Mohammad Uddin <s184174@student.dtu.dk>
2023-01-07 23:58:09 +0100

    test

--- backend/src/test/java/com/example/demo/service/UserServiceTest.java
+++ backend/src/test/java/com/example/demo/service/UserServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class UserServiceTest extends KeycloakAuthTestContainer {
@@ -21,6 +22,7 @@ class UserServiceTest extends KeycloakAuthTestContainer {
             String token = getBearer("user","user");
             List<User> userList = userService.getUsers(token);
             assertThat(userList).isNotEmpty();
+            assertEquals(userList.get(0).getClass(), User.class);
         } catch (Exception e) {
             System.out.println("Can't obtain an access token from Keycloak!");
             throw new Exception(e);
